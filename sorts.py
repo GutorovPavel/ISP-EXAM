7419c51348152c40bf02191e806012dcec184a65
GutorovPavel <pavelgutorovgut@gmail.com>
2022-06-27 19:27:17 +0300

    refactoring sorts

--- sorts.py
+++ sorts.py
@@ -61,3 +61,13 @@ def qsort(arr):
         max = [e for e in arr if e > middle]
         arr = qsort(min) + equal + qsort(max)
     return arr
+
+
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+    return arr
